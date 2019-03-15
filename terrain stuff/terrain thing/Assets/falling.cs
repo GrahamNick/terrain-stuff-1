3cc054262b5d6b8798c90cf259c0f01563a45a8b
GrahamNick <nick.g.coac@gmail.com>
2019-03-15 12:42:20 -0400

    Initial commit

---
+++ terrain stuff/terrain thing/Assets/falling.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class falling : MonoBehaviour {
+
+	private Vector3 startpos;
+
+	void Start() {
+		startpos = transform.position;
+		if (transform.position.y < -10) {
+			transform.position = startpos;
+		}
+	}
+}
+
---
+++ terrain stuff/terrain thing/Assets/triggered.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class triggered : MonoBehaviour {
+	private int count = 0;
+	public Text countText;
+	public Text winText;
+	private bool win;
+	// Use this for initialization
+	void Start () {
+		countText.text = "You have collected " + count + "/3"; 
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (count >= 3) {
+			winText.text = "You have collected all of the things";
+		}
+		changeCount ();
+
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive(false);
+			count += 1;
+		}
+	}
+	void changeCount() {
+		countText.text = "You have collected " + count + "/3";
+	}
+}
